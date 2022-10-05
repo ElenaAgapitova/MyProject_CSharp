0aa90b3f530104f5f9123b77592acf2a871c2a3e
Elena <agapitovae87@gmail.com>
2022-10-05 13:12:30 +0300

    Initial Commit

---
+++ C#turtle/TurtleTraining2/TurtleTraining2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace TurtleTraining2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Turtle.Move(100);
+
+        }
+    }
+}
---
+++ DogAndFriends/DogAndFriends/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+double distance = 1000;
+double firstFriendSpeed = 1;
+double secondFriendSpeed = 2;
+double dogSpeed = 5;
+double time=0;
+int friend = 2;
+
+while (distance>10)
+    {
+        if (friend == 1)
+        {
+            time = distance / (firstFriendSpeed + dogSpeed);
+            friend = 2; 
+        }
+        else
+        {
+            time = distance / (secondFriendSpeed + dogSpeed);
+            friend = 1;
+        }
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        count++;
+    }
+Console.WriteLine($"Ответ: собака пробежит {count} раз");
+Console.WriteLine($"Растояние между друзьями останется {distance} метров");
+Console.ReadLine();
