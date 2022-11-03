f5a481e7e02291173a00200688069ffd19cec9da
Elena <agapitovae87@gmail.com>
2022-11-03 15:32:17 +0300

    Classes GameGrid, Position added

---
+++ C#tetris/Tetris/Tetris/Block.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public abstract class Block // это абстрактный класс для каждого блока, в нем нам нужно создать двумерный массив сетки для каждого блока
+    {
+        protected abstract Position[][] Tiles { get; } // массив содержит состояние плитки блока в 4х состояниях вращения
+        protected abstract Position StarOffset { get; } // начальное смещение, которое решает, где блок появляется в сетке
+        public abstract int id { get; } // идентификатор, чтобы различать блоки (всего 7 блоков)
+
+        private int rotationState; // будем хранить текущее состояние вращения 
+        private Position offSet; // и текущее состояние смещения
+
+        // В конструкторе устанавливаем смещение равное начальному смещению
+
+        public Block()
+        {
+            offSet = new Position(StarOffset.Row, StarOffset.Column);
+        }
+
+        // Метод, который возвращает позиции сетки, занимаемые блоком с учетом текущего поворота и смещения
+
+        public IEnumerable<Position> TilePositions() // перебирает позиции плитки в текущем состоянии поворота и добавляет смещение строки и столбца
+        {
+            foreach (Position p in TilePositions())
+            {
+                yield return new Position(p.Row + offSet.Row, p.Column + offSet.Column);
+            }
+
+        }
+
+        // Метод, который поворачивает блок на 90 градусов по часовой стрелке:
+
+
+    }
+}
---
+++ C#tetris/Tetris/Tetris/GameGrid.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class GameGrid
+    {
+        private readonly int[,] grid; // сетка игры представляет собой двумерный массив из строк и столбцов
+        public int Rows { get; } // определяем свойство для количество строк
+
+        public int Columns { get; } // для столбцов
+
+        public int this[int r, int c] // индексатор для быстрого доступа к массиву
+        {
+            get => grid[r, c];
+            set => grid[r, c] = value;
+        }
+
+        public GameGrid(int rows, int columns) // можем создавать сетку для игры с любыми значениями строк и столбцов
+        {
+            Rows = rows;
+            Columns = columns;
+            grid = new int[rows, columns];
+        }
+
+        public bool IsInside(int r, int c) // метод проверяет находится ли заданная строка или столбец в игровой сетке
+        {
+            return r >= 0 && r < Rows && c >= 0 && c < Columns;
+
+        }
+
+        public bool IsEmpty(int r, int c) // проверяет пуста ли данная ячейка или нет
+        {
+            return IsInside(r,c) && grid[r,c] == 0; // ячейка должна быть в сетке и равна нулю
+        }
+
+        public bool IsRowFull(int r) // метод проверяет заполнена ли вся строка
+        {
+            for(int c = 0; c < Columns; c++)
+            {
+                if (grid[r, c] == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public bool IsRowEmpty(int r) // проверяет пуста ли строка
+        {
+            for(int c =0; c < Columns; c++)
+            {
+                if (grid[r, c] != 0)
+                    return false;
+            }
+            return true;
+        }
+
+        private void ClearRow(int r) // метод, который очищает строку, когда она полностью заполнена
+        {
+            for(int c = 0; c < Columns;c++)
+            {
+                grid[r, c] = 0;
+            }
+        }
+
+        private void MoveRowDown(int r, int numRows) // метод, который перемещает строки вниз на определенное количество строк
+        {
+            for(int c = 0; c < Columns; c++)
+            {
+                grid[r + numRows, c] = grid[r, c];
+                grid[r, c] = 0; // возможно grid[r+numRows, c] = 0
+            }
+        }
+        public int ClearFullRows() // метод очистки полных строк
+        {
+            int cleared = 0; // количество очищенных строк
+            for(int r = Rows - 1; r >= 0; r--) // начинаем с нижней строки
+            {
+                if(IsRowFull(r))
+                {
+                    ClearRow(r);
+                    cleared++;
+                }
+                else if(cleared > 0)
+                {
+                    MoveRowDown(r,cleared);
+                }
+            }
+            return cleared;
+        }
+
+    }    
+}
