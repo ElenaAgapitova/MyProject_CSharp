147bd2460e4196fded77f1d48e30e183a32361af
Elena <agapitovae87@gmail.com>
2022-11-04 14:54:42 +0300

    BlockQueue added

---
+++ C#tetris/Tetris/Tetris/BlockQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class BlockQueue // класс, который отвечает за выбор блока в игре (очередь блоков). Состоит из массива 7 блоков
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock(),
+        };
+
+        private readonly Random random = new Random();
+
+        public Block NextBlock { get; private set; } // свойство для блока
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        // Метод, который возвращает случайный блок
+        public Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];  
+        }
+        
+        // Метод, который возвращает следующий блок и обновляет свойство, чтобы не возвращать один и тот же блок
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+            do
+            {
+                NextBlock = RandomBlock();
+            }
+            while (block.id == NextBlock.id);
+
+            return block;
+
+        }
+    }
+}
---
+++ C#tetris/Tetris/Tetris/GameState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class GameState
+    {
+        // Класс состояния игры, который будет обрабатывать взаимодействия между частями написанными ранее:
+        // игоровая сетка, классы блоков и очередь блока, позиция
+
+
+    }
+}
